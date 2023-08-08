578a78c42830083504db0edff28ef133a9108457
TomMo <morettithomas54@gmail.com>
2023-08-08 15:01:46 +0200

    feat: Add open folder logic

--- src/main/kotlin/com/motompro/filesrenamer/controller/MainController.kt
+++ src/main/kotlin/com/motompro/filesrenamer/controller/MainController.kt
@@ -1,10 +1,13 @@
 package com.motompro.filesrenamer.controller
 
 import com.motompro.filesrenamer.FileRenamer
+import com.motompro.filesrenamer.FilesRenamerApplication
 import javafx.event.ActionEvent
 import javafx.fxml.FXML
 import javafx.scene.control.Button
 import javafx.scene.layout.VBox
+import javafx.scene.text.Text
+import javafx.stage.DirectoryChooser
 
 class MainController {
 
@@ -26,7 +29,19 @@ class MainController {
 
     @FXML
     private fun onOpenButtonClick(event: ActionEvent) {
-
+        val directoryChooser = DirectoryChooser()
+        directoryChooser.title = "Sélectionnez un dossier"
+        val selectedDir = directoryChooser.showDialog((event.source as Button).scene.window)
+        if (!selectedDir.isDirectory) {
+            FilesRenamerApplication.INSTANCE.showErrorAlert("Erreur", "Vous n'avez pas sélectionné un dossier")
+            return
+        }
+        val files = selectedDir.listFiles()?.filter { it.isFile } ?: emptyList()
+        if (files.isEmpty()) return
+        fileList.children.clear()
+        fileList.children.addAll(files.map { Text(it.name) })
+        fileRenamer.files.clear()
+        fileRenamer.files.addAll(files)
     }
 
     @FXML
