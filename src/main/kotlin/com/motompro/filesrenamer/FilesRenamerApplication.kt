69b31f676ff2dae5a20ba881cd275c1cc2bbbce8
TomMo <morettithomas54@gmail.com>
2023-08-12 01:21:23 +0200

    feat: Compare version with GitHub API

--- src/main/kotlin/com/motompro/filesrenamer/FilesRenamerApplication.kt
+++ src/main/kotlin/com/motompro/filesrenamer/FilesRenamerApplication.kt
@@ -1,18 +1,29 @@
 package com.motompro.filesrenamer
 
+import com.google.gson.JsonParser
 import javafx.application.Application
+import javafx.application.Platform
 import javafx.fxml.FXMLLoader
 import javafx.scene.Scene
 import javafx.scene.control.Alert
+import javafx.scene.control.ButtonBar
+import javafx.scene.control.ButtonType
 import javafx.scene.image.Image
 import javafx.stage.Stage
+import java.io.BufferedReader
 import java.io.InputStream
+import java.io.InputStreamReader
+import java.net.HttpURLConnection
+import java.net.URL
+import kotlin.concurrent.thread
 
 private const val DEFAULT_WIDTH = 720.0
 private const val DEFAULT_HEIGHT = 480.0
+private val LATEST_RELEASE_URL = URL("https://api.github.com/repos/ThomasMo54/files-renamer/releases/latest")
 
 class FilesRenamerApplication : Application() {
 
+    val version: String? = javaClass.`package`.implementationVersion
     val appIcon = Image(getResourceStream("image/app-icon.png"))
 
     init {
@@ -20,13 +31,60 @@ class FilesRenamerApplication : Application() {
     }
 
     override fun start(stage: Stage) {
-        println(FilesRenamerApplication::class.java.getResourceAsStream("/resources"))
         val fxmlLoader = FXMLLoader()
         val scene = Scene(fxmlLoader.load(getResourceStream("main-view.fxml")), DEFAULT_WIDTH, DEFAULT_HEIGHT)
-        stage.title = TITLE
+        stage.title = "$TITLE $version"
         stage.icons.add(appIcon)
         stage.scene = scene
         stage.show()
+        thread(start = true) {
+            if (version == null) return@thread
+            val latestVersion = getLatestVersion() ?: return@thread
+            if (version != latestVersion) {
+                Platform.runLater { showUpdateAlert(latestVersion) }
+            }
+        }
+    }
+
+    private fun getLatestVersion(): String? {
+        // Send API call
+        val connection = LATEST_RELEASE_URL.openConnection() as HttpURLConnection
+        connection.requestMethod = "GET"
+        connection.connectTimeout = 5000
+        connection.readTimeout = 5000
+        // Check if the call is a success
+        val status = connection.responseCode
+        if (status > 299) {
+            connection.disconnect()
+            return version
+        }
+        // Read response content
+        val reader = BufferedReader(InputStreamReader(connection.inputStream))
+        var line: String?
+        val content = StringBuffer()
+        while (run { line = reader.readLine(); line } != null) content.append(line)
+        // Close connection
+        connection.disconnect()
+        // Parse response content to get tag name
+        val jsonObject = JsonParser.parseString(content.toString()).asJsonObject
+        return jsonObject["tag_name"].asString.replace("v", "")
+    }
+
+    private fun showUpdateAlert(latestVersion: String) {
+        val yesButton = ButtonType("Oui", ButtonBar.ButtonData.YES)
+        val noButton = ButtonType("Non", ButtonBar.ButtonData.NO)
+        val alert = Alert(
+            Alert.AlertType.INFORMATION,
+            "Une nouvelle version du logiciel est disponible ($latestVersion), voulez-vous l'installer ?",
+            yesButton,
+            noButton,
+        )
+        alert.title = "Nouvelle version disponible"
+        alert.headerText = "Nouvelle version disponible"
+        val result = alert.showAndWait()
+        if (result.orElse(noButton) == yesButton) {
+
+        }
     }
 
     /**
