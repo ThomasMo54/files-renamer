7d7af487c52fb1f24c6c87f636a96c591c679b11
TomMo <morettithomas54@gmail.com>
2023-08-14 00:50:19 +0200

    feat: Added automatic upgrader

--- src/main/kotlin/com/motompro/filesrenamer/FilesRenamerApplication.kt
+++ src/main/kotlin/com/motompro/filesrenamer/FilesRenamerApplication.kt
@@ -10,16 +10,19 @@ import javafx.scene.control.ButtonBar
 import javafx.scene.control.ButtonType
 import javafx.scene.image.Image
 import javafx.stage.Stage
-import java.io.BufferedReader
-import java.io.InputStream
-import java.io.InputStreamReader
+import java.io.*
 import java.net.HttpURLConnection
 import java.net.URL
+import java.nio.file.Files
+import java.nio.file.Paths
+import java.nio.file.StandardCopyOption
 import kotlin.concurrent.thread
+import kotlin.system.exitProcess
 
 private const val DEFAULT_WIDTH = 720.0
 private const val DEFAULT_HEIGHT = 480.0
-private val LATEST_RELEASE_URL = URL("https://api.github.com/repos/ThomasMo54/files-renamer/releases/latest")
+private val LATEST_RELEASE_DATA_URL = URL("https://api.github.com/repos/ThomasMo54/files-renamer/releases/latest")
+private val LATEST_RELEASE_FILE_URL = URL("https://github.com/ThomasMo54/files-renamer/releases/latest/download/FilesRenamer.zip")
 
 class FilesRenamerApplication : Application() {
 
@@ -48,7 +51,7 @@ class FilesRenamerApplication : Application() {
 
     private fun getLatestVersion(): String? {
         // Send API call
-        val connection = LATEST_RELEASE_URL.openConnection() as HttpURLConnection
+        val connection = LATEST_RELEASE_DATA_URL.openConnection() as HttpURLConnection
         connection.requestMethod = "GET"
         connection.connectTimeout = 5000
         connection.readTimeout = 5000
@@ -73,18 +76,38 @@ class FilesRenamerApplication : Application() {
     private fun showUpdateAlert(latestVersion: String) {
         val yesButton = ButtonType("Oui", ButtonBar.ButtonData.YES)
         val noButton = ButtonType("Non", ButtonBar.ButtonData.NO)
-        val alert = Alert(
+        val askUpdateAlert = Alert(
             Alert.AlertType.INFORMATION,
             "Une nouvelle version du logiciel est disponible ($latestVersion), voulez-vous l'installer ?",
             yesButton,
             noButton,
         )
-        alert.title = "Nouvelle version disponible"
-        alert.headerText = "Nouvelle version disponible"
-        val result = alert.showAndWait()
+        askUpdateAlert.title = "Nouvelle version disponible"
+        askUpdateAlert.headerText = "Nouvelle version disponible"
+        val result = askUpdateAlert.showAndWait()
         if (result.orElse(noButton) == yesButton) {
-
+            Alert(Alert.AlertType.NONE, "Téléchargement en cours...", ButtonType.OK).show()
+            thread(start = true) { downloadLatestVersion() }
         }
+    }
+
+    private fun downloadLatestVersion() {
+        // Create file
+        val fileName = LATEST_RELEASE_FILE_URL.toString().split("/").last()
+        val filePath = "tmp${File.separator}$fileName"
+        val updateFile = File(filePath)
+        updateFile.mkdirs()
+        updateFile.createNewFile()
+
+        // Download file
+        val inputStream = BufferedInputStream(LATEST_RELEASE_FILE_URL.openStream())
+        Files.copy(inputStream, Paths.get(filePath), StandardCopyOption.REPLACE_EXISTING)
+
+        // Run the updater
+        Runtime.getRuntime().exec("cmd /c start upgrader.exe")
+
+        // Stop app
+        exitProcess(0)
     }
 
     /**
